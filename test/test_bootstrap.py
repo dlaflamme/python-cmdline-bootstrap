b3d9bc0783bf97bbe5c71c0e81707b290272a198
Daniel LaFlamme <daniel.laflamme@gmail.com>
2015-07-14 00:01:38 -0400

    add directory for test

---
+++ test/test_bootstrap.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_bootstrap
+----------------------------------
+
+Tests for `boostrap` module.
+"""
+
+import unittest
+
+sys.path.insert(0, os.path.abspath('..'))
+from .bootstrap import bootstrap
+
+
+class test_bootstrap(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
